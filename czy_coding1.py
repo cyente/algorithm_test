64508ba198d6dfcae87683d3aec209f40ea1d140
cyente <cuizeyu2016@ia.ac.cn>
2020-08-20 19:39:55 +0800

    update for all my baba

--- czy_coding1.py
+++ czy_coding1.py
@@ -3,7 +3,7 @@ import sys
 m = int(sys.stdin.readline().strip())
 coin_list = map(int, sys.stdin.readline().strip().split(' '))
 
-# with open('input1.txt', 'r') as f:
+# with open('czy_input1.txt', 'r') as f:
 #     m = int(f.readline().strip())
 #     coin_list = map(int, f.readline().strip().split(' '))
 
@@ -11,20 +11,23 @@ def share(coin_list):
     if len(coin_list) <= 1:
         return  -1
 
-
     coin_list.sort()
     sum_ = sum(coin_list)
     half = sum_ / 2.
     coin_list = coin_list[::-1]
-    x1 = 0
-    x2 = 0
-    for coin in coin_list:
-        if x1 + coin <= half:
-            x1 += coin
-        elif x2 + coin <= half:
-            x2 += coin
-        else:
-            print("some thing wrong")
+
+    dp = [[0 for _ in range(int(half))]for _ in range(m)]
+    for idx in range(m):
+        for jdx in range(int(half+1)):
+            if jdx - coin_list[idx] >= 0:
+                dp[idx][jdx] = max([dp[idx-1][jdx],
+                                    dp[idx-1][jdx - coin_list[idx]]+coin_list[idx]
+                                    ])
+            else:
+                dp[idx][jdx] = dp[idx-1][jdx]
+
+    x1 = dp[-1][-1]
+    x2 = sum_ - x1
     return abs(x1 - x2)
 
 
--- czy_coding2.py
+++ czy_coding2.py
@@ -1,15 +1,40 @@
 
-# import sys
-# n,m  = map(int, sys.stdin.readline().strip().split(" "))
-# x = [0 for _ in range(n)]
-# numsc = [[]for _ in range(n)]
-# for idx in range(n):
-#     temp = map(int, sys.stdin.readline().strip().split(" "))
-#     x[idx] = temp[0]
-#     numsc[idx] = temp[1:]
+import sys
+m = int(sys.stdin.readline().strip())
+coin_list = map(int, sys.stdin.readline().strip().split(' '))
 
-with open('input1.txt', 'r') as f:
-    n, m = map(int, f.readline().strip().split(" "))
+# with open('input1.txt', 'r') as f:
+#     m = int(f.readline().strip())
+#     coin_list = map(int, f.readline().strip().split(' '))
+
+def share(coin_list):
+    if len(coin_list) <= 1:
+        return  -1
+
+
+    coin_list.sort()
+    sum_ = sum(coin_list)
+    half = sum_ / 2.
+    coin_list = coin_list[::-1]
+    x1 = 0
+    x2 = 0
+    for coin in coin_list:
+        if x1 + coin <= half:
+            x1 += coin
+        elif x2 + coin <= half:
+            x2 += coin
+        else:
+            print("some thing wrong")
+    return abs(x1 - x2)
+
+
+print share(coin_list)
+
+
+
+
+
+
 
 
 
