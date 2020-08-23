d5464514e30311bc27e167c7b5f4394973c41153
cyente <cuizeyu2016@ia.ac.cn>
2020-08-23 20:54:40 +0800

    update for all my baba

--- czy_coding3.py
+++ czy_coding3.py
@@ -1,19 +1,7 @@
 # Definition for a binary tree node.
 
 import sys
-
-
-frame = list(map(int, sys.stdin.readline().strip().split(" ")))
-
-brick = list(map(int, sys.stdin.readline().strip().split(" ")))
-
-m, n = len(frame), len(brick)
-
-def add_brick(i, brick):
-    max_place = frame[i] + brick[0]
-    for idx in enumerate(brick):
-        if max_place < frame[i+idx] + brick[idx]:
+M = int(sys.stdin.readline().strip())
 
 
 
-for idx in range(0, m - n):
