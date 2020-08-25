18e2feab71e9993f2bbae4eb281e9ccab31170e6
cyente <cuizeyu2016@ia.ac.cn>
2020-08-25 19:48:39 +0800

    update for all my baba

--- czy_coding1.py
+++ czy_coding1.py
@@ -1,73 +1,34 @@
-from Tree import Tree
+"""
+Question 1:
+Given an array with unique integers, "compress" it, so that the minimum value is 1 and the
+maximum value is as small as possible.
 
-# import sys
-# x, opt, y = map(str, sys.stdin.readline().strip().split(' '))
-# with open('czy_input1.txt', 'r') as f:
-#     tree = map(int, f.readline().strip().split(' '))
+"Compress" rules:
 
-# root = Tree(tree[0])
+If before compression, Ai < Aj, then after compression, Ai < Aj
 
-# def read_tree(root, idx):
-#     if root == None:
-#         return
-#     if idx < len(tree):
-#         if tree[idx] == -1:
-#             root.left = None
-#         else:
-#             root.left = Tree(tree[idx])
-#     else:
-#         root.left = None
-#     idx += 1
-#     if idx < len(tree):
-#         if tree[idx] == -1:
-#             root.left = None
-#         else:
-#             root.right = Tree(tree[idx])
-#     else:
-#         root.left = None
-#     read_tree(root.left, idx)
-#     read_tree(root.right, idx)
-#     return
+e.g.
+[100, 20, 22, 30] -> [4, 1, 2, 3]
 
+format
+input: 100, 20, 22, 30
+output: 4, 1, 2, 3
+"""
 
-root = Tree(1, left=Tree(2), right=Tree(3, left=Tree(4)))
+import sys
 
+array = map(int, sys.stdin.readline().strip().split(","))
 
-# Class Tree(object):
-#     def __init(val, left=None, right=None):
-#         self.val = val
-#         self.left= left
-#         self.right = right
+def compress(arr):
+    idx_array = zip(*(range(1, len(arr)+1), array))
+    idx_array.sort(key=lambda x: x[1])
+    _index, _array = zip(*idx_array)
+    sort_idx_index = zip(*(range(1, len(arr)+1), _index))
+    sort_idx_index.sort(key=lambda x: x[1])
+    compress_list, _ = zip(*sort_idx_index)
+    return compress_list
 
-
-def cengcibianli(root):
-
-    output = [[root]]
-    current_layer = 0
-    while len(output[current_layer]) != 0:
-        output.append([])
-        for node in output[current_layer]:
-            if node.left:
-                output[-1].append(node.left)
-            if node.right:
-                output[-1].append(node.right)
-
-        current_layer += 1
-
-    output.pop()
-    for idx, line in enumerate(output):
-        output[idx] = map(lambda x: x.val, line)
-    return output
-
-
-# read_tree(root, idx=1)
-print cengcibianli(root)
-
-
-
-
-
-
+print ', '.join(map(str, compress(array)))
 
 
 
