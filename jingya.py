e5e3b6693a399b57e817f5b74618c8ba1e565fe7
jingya <ya.jing@cripac.ia.ac.cn>
2020-09-05 16:33:47 +0800

    K

--- jingya.py
+++ jingya.py
@@ -1,30 +1,56 @@
-<<<<<<< HEAD
+import numpy as np
 
-=======
->>>>>>> origin/master
-def cha(number, arr):
-    sum_arr = sum(arr)
-    lenght = len(arr)
 
-    dp = [[0 for i in range(sum_arr)] for j in range(lenght+1)]
+def normPdf(x,mu,sigma):
+    return (1./np.sqrt(2*np.pi))*(np.exp(-(x-mu)**2/(2*sigma**2)))
 
-    for i in range(1, lenght+1):
-        for j in range(1, sum_arr//2+1):
-            if j >= arr[i-1]:
-                #print(i, j, arr[i-1])
-<<<<<<< HEAD
-                dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - arr[i - 1]] +
-                               arr[i - 1])
-=======
-                dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - arr[i - 1]] + arr[i - 1])
->>>>>>> origin/master
-            else:
-                dp[i][j] = dp[i - 1][j]
 
-    return sum_arr - 2*dp[lenght][sum_arr//2]
+#K, N, M
+#K_init
+#dataarray
 
-#mm = cha(5, [5, 10, 8, 2, 10])
-mm = cha(3, [1,2,3])
-print(mm)
-print('test')
 
+
+def em(dataArray,k,mu,sigma,step):
+    #
+    n = len(k)
+    #
+    dataNum = len(dataArray)
+    #
+    gamaArray = np.zeros((n,dataNum))
+    for s in range(step):
+        for i in range(n):
+            for j in range(dataNum):
+                Sum = sum([k[t]*normPdf(dataArray[j],mu[t],sigma[t]) for t in range(n)])
+                gamaArray[i][j] = k[i]*normPdf(dataArray[j],mu[i],sigma[i])/float(Sum)
+        #
+        for i in range(n):
+            mu[i] = np.sum(gamaArray[i]*dataArray)/np.sum(gamaArray[i])
+        #
+        for i in range(n):
+            sigma[i] = np.sqrt(np.sum(gamaArray[i]*(dataArray - mu[i])**2)/np.sum(gamaArray[i]))
+        #
+        for i in range(n):
+            k[i] = np.sum(gamaArray[i])/dataNum
+
+    return [k,mu,sigma]
+
+
+
+
+
+if __name__ == '__main__':
+    #k = [0.5,0.3,0.2]
+    #mu = [0.5,5.5,7]
+    #sigma = [1,2,6]
+    #
+    dataNum = 20
+    dataArray = [0.82, 0.22, -0.03, -1.00, -0.97, 5.81, -1.50, 0.46, 4.52, 6.32, 19.44, 0.59, -10.05, -0.09, 3.49, 1.68, -1.88, 1.31, 7.75, 2.02]
+    k0 = [0.5,0.3,0.2]
+    mu0 = [0.5,5.5,7]
+    sigma0 = [1,2,6]
+    step = 3
+    k1,mu1,sigma1 = em(dataArray,k0,mu0,sigma0,step)
+    print("k1:",k1)
+    print("mu1:",mu1)
+    print("sigma1:",sigma1)
